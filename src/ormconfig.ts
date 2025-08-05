b7478e32407537d6ceb6e3c852faeb2a8e26b5df
norneticsm <rafihilmifirdaus@gmail.com>
2025-08-05 15:10:52 +0700

    updated config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'system',
-  database: 'blog',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   // synchronize: true, // dont using for production!
   migrationsTableName: 'migrations',
